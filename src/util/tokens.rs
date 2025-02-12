b7eb8b39dae3c0d9fcc7bf19e524e54fe85028fb
john <johnathan.stephens@protonmail.com>
2025-02-12 05:54:16 +0100

    expand tokens template

--- src/util/tokens.rs
+++ src/util/tokens.rs
@@ -1,3 +1,23 @@
 pub struct Tokens {
-    
+    keywords: Keywords,
+    identifiers: Identifiers,
+    literals: Literals,
+    symbols: Symbols
+}
+
+
+pub struct Keywords {
+
+}
+
+pub struct Identifiers {
+
+}
+
+pub struct Literals {
+
+}
+
+pub struct Symbols {
+
 }
