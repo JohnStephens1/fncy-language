dfaaa7706cc54d1c89d83983c1a00f7130836cf8
john <johnathan.stephens@protonmail.com>
2025-02-23 15:25:40 +0100

    [wip] clean up most of fun file

--- src/types/fun.rs
+++ src/types/fun.rs
@@ -1,74 +1,76 @@
 use super::var::Var;
+use crate::util::processing;
 
 
 #[derive(Debug)]
 pub struct Fun {
     pub name: String,
-    pub parameters: Vec<Parameter>,
+    pub parameters: Vec<Var>,
     pub return_type: String,
     pub code: Vec<String>
 }
+// todo impl get return type
 
 
 // todo fixed, but still hideous xd feel free to update
-fn get_parameters(mut params: &[String]) -> Vec<Var> {
-    params = &params[1..params.len() - 1];
+// fn get_parameters_old(mut params: &[String]) -> Vec<Var> {
+//     params = &params[1..params.len() - 1];
 
-    let mut parameters: Vec<Var> = Vec::new();
+//     let mut parameters: Vec<Var> = Vec::new();
 
-    let mut comma_index: Vec<usize> = Vec::new();
-    let mut colon_index: Vec<usize> = Vec::new();
-    let mut equals_index: Vec<usize> = Vec::new();
+//     let mut comma_index: Vec<usize> = Vec::new();
+//     let mut colon_index: Vec<usize> = Vec::new();
+//     let mut equals_index: Vec<usize> = Vec::new();
 
-    let mut default_value: Vec<String> = vec!["".to_string()];
+//     let mut default_value: Vec<String> = vec!["".to_string()];
 
-    let mut i: usize = 0;
-    for string in params {
-        match string.as_str() {
-            "," => comma_index.push(i),
-            ":" => colon_index.push(i),
-            "=" => equals_index.push(i),
-            _ => {}
-        }
+//     let mut i: usize = 0;
+//     for string in params {
+//         match string.as_str() {
+//             "," => comma_index.push(i),
+//             ":" => colon_index.push(i),
+//             "=" => equals_index.push(i),
+//             _ => {}
+//         }
 
-        i += 1;
-    }
+//         i += 1;
+//     }
 
-    i = 0;
-    let mut i_equals: i32 = 0;
-    for colon_idx in &colon_index {
-        if let Some(equals_idx) = equals_index.get(i_equals as usize) {
-            if let Some(comma_idx) = comma_index.get(i) {
-                if comma_idx < equals_idx {
-                    i_equals -= 1;
-                } else {
-                    default_value = params[*equals_idx + 1..*comma_idx].join(" ");
-                }
-            } else {
-                default_value = params[*equals_idx + 1..];
-            }
-        }
+//     i = 0;
+//     let mut i_equals: i32 = 0;
+//     for colon_idx in &colon_index {
+//         if let Some(equals_idx) = equals_index.get(i_equals as usize) {
+//             if let Some(comma_idx) = comma_index.get(i) {
+//                 if comma_idx < equals_idx {
+//                     i_equals -= 1;
+//                 } else {
+//                     default_value = params[*equals_idx + 1..*comma_idx].join(" ");
+//                 }
+//             } else {
+//                 default_value = params[*equals_idx + 1..];
+//             }
+//         }
 
-        parameters.push(
-            Var::new(
-                params[colon_idx - 1].clone(),
-                params[colon_idx + 1].clone(),
-                default_value.clone()
-            ));
+//         parameters.push(
+//             Var::new(
+//                 params[colon_idx - 1].clone(),
+//                 params[colon_idx + 1].clone(),
+//                 default_value.clone()
+//             ));
 
-        i_equals += 1;
-        i += 1;
-        default_value = vec!["".to_string()];
-    }
+//         i_equals += 1;
+//         i += 1;
+//         default_value = vec!["".to_string()];
+//     }
 
-    parameters
-}
+//     parameters
+// }
 
 // how bout we write a cleaner one xd
-fn get_parameters_new(mut slice: &[String]) -> Vec<Var> {
+fn get_parameters(mut slice: &[String]) -> Vec<Var> {
     slice = &slice[1..slice.len() - 1];
     let i: usize = 0;
-    
+
     let mut params: Vec<Var> = Vec::new();
     let mut expression: Vec<String> = Vec::new();
     let mut name: String = "".to_string();
@@ -131,7 +133,7 @@ fn get_fun(slice: &[String]) {
 
 }
 
-fn fun_def_handler(code: &[String]) -> (types::Fun, usize) {
+fn fun_def_handler(code: &[String]) -> (Fun, usize) {
     if code.first().expect("no fun, nothing at all tbh") != "fun" { panic!("no fun indeed") };
 
     let fun_name = code[1].clone();
@@ -147,7 +149,7 @@ fn fun_def_handler(code: &[String]) -> (types::Fun, usize) {
     let end_of_code = start_of_code + processing::get_i_of_next_matching_brace(&code[start_of_code..]);
     let fun_code = code[start_of_code..=end_of_code].to_vec();
 
-    let my_fun = types::Fun {
+    let my_fun = Fun {
         name: fun_name,
         parameters: fun_params,
         return_type,
