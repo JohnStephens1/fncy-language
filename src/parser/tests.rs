925ba0ad84226d5aab35a0c4997e158b6d0f36b8
john <johnathan.stephens@protonmail.com>
2025-02-25 04:29:22 +0100

    merge test_find_end_of_expression

--- src/parser/tests.rs
+++ src/parser/tests.rs
@@ -3,38 +3,28 @@ use pretty_assertions::assert_eq;
 use super::parser;
 
 
-// todo
-// kinda wanna merge this
-// extremely ugly and verbose xd
 #[test]
-fn test_find_end_of_expression_1() {
-    let test_1: Vec<String> = "something . something ( hell hello hello ) . guten_tag shoulda_stopped_here hello ( )"
-        .split_ascii_whitespace().into_iter().map(String::from).collect();
+fn test_find_end_of_expression() {
+    let tests: Vec<Vec<String>> = [
+        "something . something ( hell hello hello ) . guten_tag shoulda_stopped_here hello ( )",
+        "something something ( hell hello hello ) . guten_tag shoulda_stopped_here hello ( )"
+    ].into_iter().map(|x| x.split_ascii_whitespace().into_iter().map(String::from).collect()).collect();
+    
+    let expected_matches: Vec<Vec<String>> = [
+        "something . something ( hell hello hello ) . guten_tag",
+        "something"
+    ].into_iter().map(|x| x.split_ascii_whitespace().into_iter().map(String::from).collect::<Vec<String>>()).collect();
 
-    let test_1_expected_match: Vec<String> = "something . something ( hell hello hello ) . guten_tag"
-        .split_ascii_whitespace().into_iter().map(String::from).collect();
+    let expected_remainders: Vec<Vec<String>> = [
+        "shoulda_stopped_here hello ( )",
+        "something ( hell hello hello ) . guten_tag shoulda_stopped_here hello ( )"
+    ].into_iter().map(|x| x.split_ascii_whitespace().into_iter().map(String::from).collect::<Vec<String>>()).collect();
 
-    let test_1_expected_remainder: Vec<String> = "shoulda_stopped_here hello ( )"
-        .split_ascii_whitespace().into_iter().map(String::from).collect();
-
-    let (le_match, remainder) = parser::find_end_of_expression(&test_1);
-    assert_eq!(&test_1_expected_match, &le_match);
-    assert_eq!(&test_1_expected_remainder, &remainder);
+    let (le_matches, remainders): (Vec<Vec<String>>, Vec<Vec<String>>) = tests.iter().map(parser::find_end_of_expression).unzip();
+    assert_eq!(&expected_matches, &le_matches);
+    assert_eq!(&expected_remainders, &remainders);
 }
 
-#[test]
-fn test_find_end_of_expression_2() {
-    let test_2: Vec<String> = "something something ( hell hello hello ) . guten_tag shoulda_stopped_here hello ( )"
-        .split_ascii_whitespace().into_iter().map(String::from).collect();
-
-    let test_2_expected_match: Vec<String> = ["something"].into_iter().map(String::from).collect();
-    let test_2_expected_remainder: Vec<String> = "something ( hell hello hello ) . guten_tag shoulda_stopped_here hello ( )"
-        .split_ascii_whitespace().into_iter().map(String::from).collect();
-
-    let (le_match, remainder) = parser::find_end_of_expression(&test_2);
-    assert_eq!(&test_2_expected_match, &le_match);
-    assert_eq!(&test_2_expected_remainder, &remainder);
-}
 
 // todo
 // mut x without & mut x can cause issues in some cases
